fdd947bda883493a49376e3b1d7da7f476504d85
peterortiz82 <peterortiz82@gmail.com>
2015-10-18 11:50:09 -0400

    added specs for create method and generated the SessionsController with route.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+
+  post 'auth/twitter/callback', to: 'sessions#create'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
---
+++ spec/controllers/sessions_controller_spec.rb
@@ -0,0 +1,40 @@
+require 'rails_helper'
+
+describe SessionsController do
+
+  before do
+    request.env["omniauth.auth"] = OmniAuth.config.mock_auth[:twitter]
+  end
+
+  describe "#create" do
+
+    subject { post :create, provider: :twitter }
+
+    it "successfully create a user" do
+      expect(subject).to change{ User.count }.by(1)
+    end
+
+    it "successfully create a session" do
+      expect(session[:user_id]).to eq nil
+      subject
+      expect(session[:user_id]).not_to eq nil
+    end
+
+    it "redirect the user to the root url" do
+      subject
+      expect(response).to redirect_to root_url
+    end
+
+  end
+
+  describe "#destroy" do
+
+    subject { post :create, provider: :twitter }
+
+    it "clears the session"
+
+    it "redirect to the home page"
+
+  end
+
+end
