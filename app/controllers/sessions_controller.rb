58b9d29dd7e7a2fa407e96dd450650d15e181213
peterortiz82 <peterortiz82@gmail.com>
2015-10-18 21:05:49 -0400

    added signin/signout with twitter  functionality and basic styling with bootstrap.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,12 +3,12 @@ class SessionsController < ApplicationController
   def create
     user = User.find_or_create_from_auth_hash(auth_hash)
     session[:user_id] = user.id
-    redirect_to root_path
+    redirect_to root_path, notice: 'You have signed in successfully!'
   end
 
   def destroy
     session[:user_id] = nil
-    redirect_to root_path
+    redirect_to root_path, notice: 'You have signed out.'
   end
 
   private
--- spec/features/welcome_index_spec.rb
+++ spec/features/welcome_index_spec.rb
@@ -6,21 +6,38 @@ describe 'Welcome index' do
     visit root_path
   end
 
-  it 'has a Sign in with Twitter button'
+  it 'has a Sign in with Twitter button' do
+    expect(page).to have_content 'Sign in with Twitter'
+  end
 
   context 'sign in' do
+    let(:user) { create :user }
 
-  it 'allows a user to sign in with their twitter credentials'
+    before do
+      click_on 'Sign in with Twitter'
+    end
 
-  it 'redirects user to the root path and displays a success message'
+    it 'redirects user to the root path and displays a success message' do
+      expect(current_path).to eq root_path
+      expect(page).to have_content 'You have signed in successfully!'
+    end
 
-  it 'displays the users name and a Sign Out link in the navbar'
+    it 'displays the users image and a Sign Out link in the navbar' do
+      expect(page).to have_content user.image
+      expect(page).to have_content 'Sign Out'
+    end
 
+    it 'does not show the Sign in with Twitter button' do
+      expect(page).not_to have_content 'Sign in with Twitter'
+    end
   end
 
   context 'sign out' do
-
-    it 'redirects user to the root path and displays a sign out notice'
-
+    it 'redirects user to the root path and displays a sign out notice' do
+      click_on 'Sign in with Twitter'
+      click_on 'Sign Out'
+      expect(current_path).to eq root_path
+      expect(page).to have_content 'You have signed out.'
+    end
   end
 end
