0baef105db9df0bb8d6c20051108e2c2d1822eb6
peterortiz82 <peterortiz82@gmail.com>
2015-10-20 22:32:22 -0400

    created form to post a tweet locally and added TweetHelper to prevent anyone who is not signed in access to post a tweet.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,16 +1,20 @@
 class TweetsController < ApplicationController
+  include TweetsHelper
+  before_action :signed_in_user
 
   def index
+    @tweets = Tweet.all
   end
 
   def new
+    @tweet = Tweet.new
   end
 
   def create
     @tweet = Tweet.new tweet_params
     @tweet.user_id = current_user.id
     if @tweet.save
-      redirect_to tweets_path
+      redirect_to tweets_path, notice: 'Your tweet has been posted!'
     else
       render :new
     end
--- spec/features/post_tweet_spec.rb
+++ spec/features/post_tweet_spec.rb
@@ -1,25 +1,55 @@
 require 'rails_helper'
 
 describe 'Posting a tweet' do
-  let(:user) { create :user }
-  let(:tweet) { create :tweet, user: user }
-
   describe 'new page' do
-    it 'shows Compose a new Tweet'
-    it 'has a text box to enter tweet content'
+    let(:user) { create :user }
+    let(:tweet) { create :tweet, user: user }
+
+    before {
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+      visit new_tweet_path
+    }
+
+    it 'shows Compose a new Tweet' do
+      expect(page).to have_content 'Compose new Tweet'
+    end
+
+    it 'has a text box to enter tweet content' do
+      expect(page).to have_selector :text, '#tweet_tweet_body'
+    end
 
     describe 'Form submission' do
       context 'with no content' do
-        it 'renders new and shows error message'
+        it 'renders new and shows error message' do
+          click_on 'Tweet'
+          expect(current_path).to eq tweets_path
+          expect(page).to have_content "Tweet body can't be blank"
+        end
       end
 
       context 'when posting a tweet that is more than 140 characters' do
-        it 'renders new and shows error message'
+        it 'renders new and shows error message' do
+          fill_in 'Compose new Tweet', with: "a"*141
+          click_on 'Tweet'
+          expect(page).to have_content 'Tweet body is too long (maximum is 140 characters)'
+        end
       end
 
       context 'with correct content' do
-        it 'successfully creates a tweet'
-        it 'redirect you to the tweets index page and displays the tweet'
+        before {
+          fill_in 'Compose new Tweet', with: tweet.tweet_body
+          click_on 'Tweet'
+        }
+
+        it 'successfully creates a tweet' do
+          expect(page).to have_content 'Your tweet has been posted!'
+        end
+
+        it 'redirect you to the tweets index page and displays the tweet' do
+          expect(current_path).to eq tweets_path
+          expect(page).to have_content tweet.user.name
+          expect(page).to have_content tweet.tweet_body
+        end
       end
     end
   end
